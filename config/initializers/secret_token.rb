246288be7e27192c5388a030db2e27df1099573d
GrimmOutlook <grimmoutlook87@gmail.com>
2013-12-05 16:38:22 -0500

    Intitial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Caphoto::Application.config.secret_key_base = 'f8c33787d0c9b855ae79d21a344d25a6f09fcc715c2f824235d5f47fb513c3b4abb0a8e7cd0b12624aa53679149c497e215ef58eccd535207f91afd09077f379'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Caphoto::Application.config.session_store :cookie_store, key: '_caphoto_session'
